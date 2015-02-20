79054e02dbe84dd85a74cadc127764546a16eb06
K McHugh <kmchugh@Office.local>
2015-02-20 12:12:52 +0800

    cleanup of build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,23 +12,36 @@ module.exports = function(grunt) {
         // Where we will store the html,js for test pages
         test: 'src/test/',
         // Where we will place the final distribution files
-        dist: 'dist/'
+        dist: 'dist/',
+        // The library files
+        libs: 'src/libs/',
+        // The script files
+        scripts: 'src/scripts/',
+        // The script files
+        resources: 'src/resources/',
+        // The node modules
+        nodeModules: 'node_modules/'
     };
 
     var livereloadPort = 35730;
 
-//    paths.roots.views = paths.roots.app + 'Offline/';
+    //    paths.roots.views = paths.roots.app + 'Offline/';
 
     // Distribution paths
     paths.dist = {
         less: paths.roots.dist + 'less/',
         css: paths.roots.dist + 'css/',
-        resources: paths.roots.dist + 'resources'
+        resources: paths.roots.dist + 'resources/',
+        fonts: paths.roots.dist + 'fonts/',
+        scripts: paths.roots.dist + 'scripts/',
     };
 
     // Build paths
     paths.build = {
-        css: paths.roots.build + 'css/'
+        css: paths.roots.build + 'css/',
+        fonts: paths.roots.build + 'fonts/',
+        scripts: paths.roots.build + 'scripts/',
+        resources: paths.roots.build + 'resources/',
     };
 
     grunt.initConfig({
@@ -39,7 +52,7 @@ module.exports = function(grunt) {
             // Recompile the .css if there are any changes to any of the .less files
             styles: {
                 files: [paths.roots.styles + '{,*/}*.less'],
-                tasks: ['clean:styles', 'less'],
+                tasks: ['build'],
                 options: {
                     livereload: livereloadPort
                 }
@@ -47,7 +60,15 @@ module.exports = function(grunt) {
             // Changes to any of the test files should also update
             tests: {
                 files: [paths.roots.test + '{,*/}*.*'],
-                tasks: ['clean', 'build'],
+                tasks: ['build'],
+                options: {
+                    livereload: livereloadPort
+                }
+            },
+            // Changes to any of the scripts should reload
+            scripts: {
+                files: [paths.roots.scripts + '{,*/}*.js'],
+                tasks: ['build'],
                 options: {
                     livereload: livereloadPort
                 }
@@ -55,7 +76,8 @@ module.exports = function(grunt) {
 
         },
 
-        connect:{
+        // Connect and serve the test pages
+        connect: {
             server: {
                 options: {
                     open: {
@@ -64,6 +86,19 @@ module.exports = function(grunt) {
                     base: paths.roots.build,
                     livereload: livereloadPort
                 }
+            }
+        },
+
+        // Minify the css
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: paths.build.css,
+                    src: ['*.css', '!*.min.css', '!*test.css'],
+                    dest: paths.dist.css,
+                    ext: '.less.min.css'
+                }]
             }
         },
 
@@ -86,8 +121,7 @@ module.exports = function(grunt) {
                     src: '{,**/}[^_]*.less',
                     dest: paths.build.css,
                     ext: '.less.css'
-                },
-                {
+                }, {
                     expand: true,
                     cwd: paths.roots.test,
                     src: '{,**/}[^_]*.less',
@@ -100,25 +134,39 @@ module.exports = function(grunt) {
         // Cleanup task where to clean out the dist folder
         clean: {
             build: paths.roots.build,
-            dist: paths.roots.dist,
-            styles: paths.dist.css
+            dist: paths.roots.dist
         },
 
+        // Minify any images
         imagemin: {
-            dist: {
+            main: {
                 files: [{
                     expand: true,
-                    cwd: paths.roots.styles,
-                    src: ['{,**/}*.{png,jpg,gif}'],
-                    dest: paths.dist.resources
+                    cwd: paths.roots.resources,
+                    src: ['{,**/}*.{png,jpg,gif,icns,ico}'],
+                    dest: paths.build.resources
                 }]
             }
         },
 
+        // Minify the js
+        uglify : {
+            dist : {
+                files : [{
+                    expand: true,
+                    cwd: paths.build.scripts,
+                    src: '*.js',
+                    dest: paths.dist.scripts,
+                    ext: '.min.js'
+                }]
+            }
+        },
+
+        // Ensure all files are copied
         copy: {
             main: {
                 files: [
-                    // Copy the test files to the build folder
+                    // Copy the test index files to the build folder
                     {
                         expand: true,
                         cwd: paths.roots.test,
@@ -126,17 +174,63 @@ module.exports = function(grunt) {
                         dest: paths.roots.build,
                         flatten: true
                     },
+                    // Copy the fonts
+                    {
+                        expand: true,
+                        cwd: paths.roots.libs + 'font-awesome/fonts/',
+                        src: ['*'],
+                        dest: paths.build.fonts,
+                        flatten: true
+                    },
+                    // Copy the fonts
+                    {
+                        expand: true,
+                        cwd: paths.roots.nodeModules + 'bootstrap/fonts',
+                        src: ['*'],
+                        dest: paths.build.fonts,
+                        flatten: true
+                    },
+                    // Copy the scripts
+                    {
+                        expand: true,
+                        cwd: paths.roots.nodeModules + 'bootstrap/dist/js',
+                        src: ['bootstrap.js'],
+                        dest: paths.build.scripts,
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        cwd: paths.roots.scripts,
+                        src: ['*.js'],
+                        dest: paths.build.scripts,
+                        flatten: true
+                    }
+                ]
+            },
+            dist: {
+                files: [
+                    // Copy the less files to the dist folder
                     {
                         expand: true,
                         cwd: paths.roots.styles,
                         src: ['*.less'],
                         dest: paths.dist.less,
                     },
+                    // Copy the fonts
                     {
                         expand: true,
-                        cwd: paths.build.css,
-                        src: ['effort.less.css'],
-                        dest: paths.dist.css,
+                        cwd: paths.build.fonts,
+                        src: ['*'],
+                        dest: paths.dist.fonts,
+                        flatten: true
+                    },
+                    // Copy the resources
+                    {
+                        expand: true,
+                        cwd: paths.build.resources,
+                        src: ['*'],
+                        dest: paths.dist.resources,
+                        flatten: true
                     }
                 ]
             }
@@ -147,14 +241,14 @@ module.exports = function(grunt) {
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     //Dev task, use this task for developing real-time
-    grunt.registerTask('dev', ['clean', 'imagemin:dist', 'less', 'copy', 'connect', 'watch']);
+    grunt.registerTask('dev', ['dist', 'connect', 'watch']);
 
     // Build task
-    grunt.registerTask('build', ['newer:imagemin:dist', 'newer:less', 'newer:copy']);
+    grunt.registerTask('dist', ['clean', 'build']);
 
     // Build task
-    grunt.registerTask('dist', ['clean', 'build', 'uglify', 'copy']);
+    grunt.registerTask('build', ['newer:imagemin', 'less', 'newer:cssmin', 'newer:copy', 'newer:uglify']);
 
     // Default task
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['dev']);
 };
